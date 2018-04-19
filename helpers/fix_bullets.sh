b5b8d02c34cdbc7268fb72ff36f002669679833e
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2018-04-19 11:24:36 +0100

    upd

--- helpers/fix_bullets.sh
+++ helpers/fix_bullets.sh
@@ -4,10 +4,14 @@ IN_FILE="$1"
 OUT_FILE="$2"
 
 cat "$IN_FILE" |\
+    # convert ḍ, ḷ, Ā, throws off perl regex
+    sed -e 's/ḍ/XdX/g; s/ḷ/XlX/g; s/Ā/XAX/g;' |\
     # extra linebreaks
     sed 's/^• /\n\n\n\n&/' |\
     # with reference
     perl -0777 -pe "s/\n\n• +([\w'\"[:punct:]-]+[^•]*) \+\n_(\w+[^•_]*)_ *\n{0,1}(\(([^\)]+)\)([[:punct:]]?)) *\n\n/\n\n[quote, \4]\n____\n\1\n\n_\2\5_\n____\n\n/g" |\
     # without reference
     perl -0777 -pe "s/\n\n• +([\w'\"[:punct:]-]+[^•]*) \+\n_(\w+[^•_]*)_([[:punct:]]?)\n\n/\n\n____\n\1\n\n_\2\3_\n____\n\n/g" |\
+    # convert back ḍ, ḷ, Ā
+    sed -e 's/XdX/ḍ/g; s/XlX/ḷ/g; s/XAX/Ā/g;' |\
     cat -s > "$OUT_FILE"
--- helpers/join_quotes.sh
+++ helpers/join_quotes.sh
@@ -4,6 +4,8 @@ IN_FILE="$1"
 OUT_FILE="$2"
 
 cat "$IN_FILE" |\
+    # convert ḍ, ḷ, Ā, throws off perl regex
+    sed -e 's/ḍ/XdX/g; s/ḷ/XlX/g; s/Ā/XAX/g;' |\
     perl -0777 -pe "s/\n\n____\n(((?!____|====).)+)\n____\n\n([[]quote, [^\n]+[]]\n____\n)/\n\n\3\1\n\n/gs" |\
     perl -0777 -pe "s/\n\n____\n(((?!____|====).)+)\n____\n\n([[]quote, [^\n]+[]]\n____\n)/\n\n\3\1\n\n/gs" |\
     perl -0777 -pe "s/\n\n____\n(((?!____|====).)+)\n____\n\n([[]quote, [^\n]+[]]\n____\n)/\n\n\3\1\n\n/gs" |\
@@ -44,5 +46,8 @@ cat "$IN_FILE" |\
     perl -0777 -pe "s/\n\n____\n(((?!____|====).)+)\n____\n\n([[]quote, [^\n]+[]]\n____\n)/\n\n\3\1\n\n/gs" |\
     perl -0777 -pe "s/\n\n____\n(((?!____|====).)+)\n____\n\n([[]quote, [^\n]+[]]\n____\n)/\n\n\3\1\n\n/gs" |\
     perl -0777 -pe "s/\n\n____\n(((?!____|====).)+)\n____\n\n([[]quote, [^\n]+[]]\n____\n)/\n\n\3\1\n\n/gs" |\
-    perl -0777 -pe "s/\n\n____\n(((?!____|====).)+)\n____\n\n([[]quote, [^\n]+[]]\n____\n)/\n\n\3\1\n\n/gs" > "$OUT_FILE"
+    perl -0777 -pe "s/\n\n____\n(((?!____|====).)+)\n____\n\n([[]quote, [^\n]+[]]\n____\n)/\n\n\3\1\n\n/gs" |\
+    # convert back ḍ, ḷ, Ā
+    sed -e 's/XdX/ḍ/g; s/XlX/ḷ/g; s/XAX/Ā/g;' |\
+    cat -s > "$OUT_FILE"
 
